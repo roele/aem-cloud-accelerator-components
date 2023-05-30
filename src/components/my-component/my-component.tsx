45af66326541dd22dfcb6d4ded1bf38e9145a79b
Yves Tscherry <info@feerglas.com>
2023-05-30 09:40:43 +0200

    feat: add sample component

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,37 @@
+import {
+  Component,
+  h,
+  Prop
+} from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  shadow: true,
+  styleUrl: 'my-component.css',
+  tag: 'my-component'
+})
+export class MyComponent {
+
+  /**
+   * The first name
+   */
+  @Prop() public first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() public middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() public last: string;
+
+  private _getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  public render(): HTMLDivElement {
+    return <div>Hello, World! I'm {this._getText()}</div>;
+  }
+}
