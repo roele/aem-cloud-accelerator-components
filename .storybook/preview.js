d94be71a2db4153b44ffca9e2a179a5cf4a25667
Yves Tscherry <info@feerglas.com>
2023-05-30 11:26:25 +0200

    chore: use typescript

--- .storybook/preview.js
+++
@@ -1,3 +0,0 @@
-import {defineCustomElements} from '../dist/esm/loader';
-
-defineCustomElements();
---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import type { Preview } from '@storybook/html';
+import {defineCustomElements} from '../dist/esm/loader';
+
+defineCustomElements();
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
