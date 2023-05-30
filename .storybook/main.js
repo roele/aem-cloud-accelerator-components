7ef590dc5add099db05ef3d712690c42a466018a
Yves Tscherry <info@feerglas.com>
2023-05-30 10:25:18 +0200

    chore: add storybook config files

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: [
+    '../src/**/*.stories.js',
+    '../src/**/*.stories.mdx'
+  ],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@pxblue/storybook-rtl-addon/register',
+  ],
+  features: {
+    postcss: false
+  }
+};
---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+let title = 'AEM Cloud Accelerator Components';
+
+export default create({
+  base: 'light',
+  brandTitle: title
+});
